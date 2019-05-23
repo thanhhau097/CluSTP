d409b3981546542de8374a0dedef7d59efa5cac9
Lionel <lionel@cinnamon.is>
2019-05-23 11:06:34 +0700

    update code for cluster

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class CluSTP:
 
         # need to update in init_solution and set_value_propagate
         self.x = np.zeros([self.n, self.n])
-        # cost[i]: cost of vertice i
+        # cost[i]: cost of vertex i
         self.cost = np.zeros(self.n)
         # n_out[i] numbers of out-edge of cluster i
         # self.n_out = np.zeros(self.n_clusters)
@@ -70,17 +70,17 @@ class CluSTP:
             open_set = Ri
             close_set = set()
 
-            first_vertice = random.sample(open_set, 1)[0]
-            open_set.remove(first_vertice)
-            close_set.add(first_vertice)
+            first_vertex = random.sample(open_set, 1)[0]
+            open_set.remove(first_vertex)
+            close_set.add(first_vertex)
 
             while len(open_set) != 0:
-                random_close_vertice = random.sample(close_set, 1)[0]
-                random_open_vertice = random.sample(open_set, 1)[0]
-                self.add_edge(random_close_vertice, random_open_vertice)
+                random_close_vertex = random.sample(close_set, 1)[0]
+                random_open_vertex = random.sample(open_set, 1)[0]
+                self.add_edge(random_close_vertex, random_open_vertex)
 
-                open_set.remove(random_open_vertice)
-                close_set.add(random_open_vertice)
+                open_set.remove(random_open_vertex)
+                close_set.add(random_open_vertex)
 
         # 2. nối các cụm chưa được nối với cụm nào vào các cụm đã nối
         # add edges from cluster to cluster
@@ -94,14 +94,14 @@ class CluSTP:
 
         while (len(open_set)) != 0:
             random_close_cluster = random.sample(close_set, 1)[0]
-            random_close_vertice = random.sample(self.R[random_close_cluster], 1)[0]
+            random_close_vertex = random.sample(self.R[random_close_cluster], 1)[0]
 
             random_open_cluster = random.sample(open_set, 1)[0]
-            random_open_vertice = random.sample(self.R[random_open_cluster], 1)[0]
+            random_open_vertex = random.sample(self.R[random_open_cluster], 1)[0]
 
-            self.add_edge(random_close_vertice, random_open_vertice)
+            self.add_edge(random_close_vertex, random_open_vertex)
 
-#             print('add', random_close_vertice, random_open_vertice)
+#             print('add', random_close_vertex, random_open_vertex)
             open_set.remove(random_open_cluster)
             close_set.add(random_open_cluster)
 
@@ -186,39 +186,42 @@ class CluSTP:
         nx.draw(G, labels=labelmap, with_labels=True)
         plt.show()
 
-    def get_assign_delta(self, out_vertice, connected_vertice, new_out_vertice, new_connected_vertice):
-        # return changed value of just one vertice in that cluster
-        old_edge_distance = self.distances[out_vertice, connected_vertice]
-        new_edge_distance = self.distances[new_out_vertice, new_connected_vertice]
+    def get_assign_delta(self, out_vertex, connected_vertex, new_out_vertex, new_connected_vertex):
+        # return changed value of just one vertex in that cluster
+        old_edge_distance = self.distances[out_vertex, connected_vertex]
+        new_edge_distance = self.distances[new_out_vertex, new_connected_vertex]
 
-        # n_vertices_in_leaf_cluster = len(self.R[int(self.cluster_of_vertices[out_vertice])])
+        # n_vertices_in_leaf_cluster = len(self.R[int(self.cluster_of_vertices[out_vertex])])
         return (new_edge_distance - old_edge_distance) #* n_vertices_in_leaf_cluster
 
-    def set_value_propagate(self, out_vertice, connected_vertice, new_out_vertice, new_connected_vertice):
+    def set_value_propagate(self, out_vertex, connected_vertex, new_out_vertex, new_connected_vertex):
         # update x, out_vertices_of_cluster
-        self.remove_edge(out_vertice, connected_vertice)
-        self.add_edge(new_out_vertice, new_connected_vertice)
+        self.remove_edge(out_vertex, connected_vertex)
+        self.add_edge(new_out_vertex, new_connected_vertex)
 
         # update cost, total cost
-        vertices_in_leaf_cluster = self.R[int(self.cluster_of_vertices[out_vertice])]
-        delta = self.get_assign_delta(out_vertice, connected_vertice, new_out_vertice, new_connected_vertice)
-        for vertice in vertices_in_leaf_cluster:
-            self.cost[vertice] += delta
+        vertices_in_leaf_cluster = self.R[int(self.cluster_of_vertices[out_vertex])]
+        delta = self.get_assign_delta(out_vertex, connected_vertex, new_out_vertex, new_connected_vertex)
+        for vertex in vertices_in_leaf_cluster:
+            self.cost[vertex] += delta
 
         self.total_cost += delta * len(vertices_in_leaf_cluster)
 
     def get_neighbors(self):
-        # chọn cụm bất kỳ có cạnh ra bằng 1: cụm lá
+        # chọn cụm bất kỳ có cạnh ra bằng 1: cụm lá // hiện tại không lại cụm chứa đỉnh gốc vì có khó
+        # khăn ở bước thay đổi cost
         leaf_clusters = [i for i in range(self.n_clusters) if len(self.out_vertices_of_cluster[i]) == 1] #np.where(self.n_out == 1)[0]
+        if self.cluster_of_vertices[self.source_vertex] in leaf_clusters:
+            leaf_clusters.remove(self.cluster_of_vertices[self.source_vertex])
         random_cluster = np.random.choice(leaf_clusters)
 
         out_vertices = self.out_vertices_of_cluster[random_cluster]
-        out_vertice = random.sample(out_vertices, 1)[0]
-        connected_vertices = np.where(self.x[out_vertice, :] == 1)[0]
-        # must choose vertice that are not in the same cluster
+        out_vertex = random.sample(out_vertices, 1)[0]
+        connected_vertices = np.where(self.x[out_vertex, :] == 1)[0]
+            # must choose vertex that are not in the same cluster
         connected_vertices = set(connected_vertices).difference(self.R[random_cluster])
-        connected_vertice = random.sample(connected_vertices, 1)[0]
-        # current_connected_cluster = self.cluster_of_vertices[connected_vertice]
+        connected_vertex = random.sample(connected_vertices, 1)[0]
+        # current_connected_cluster = self.cluster_of_vertices[connected_vertex]
         # other_clusters = list(range(self.n_clusters)).remove(current_connected_cluster)
 
         # lấy một đỉnh ở trong random_cluster, nối với những đỉnh bất kỳ khác cụm
@@ -226,13 +229,53 @@ class CluSTP:
         other_vertices = set(range(self.n)).difference(cluster_vertices)
 
         new_combination_vertices = []
-        for new_out_vertice in cluster_vertices:
-            for new_connected_vertice in other_vertices:
-                if self.cluster_of_vertices[new_out_vertice] == self.cluster_of_vertices[new_connected_vertice]:
-                    print(1)
-                new_combination_vertices.append((new_out_vertice, new_connected_vertice))
+        for new_out_vertex in cluster_vertices:
+            for new_connected_vertex in other_vertices:
+                # if self.cluster_of_vertices[new_out_vertex] == self.cluster_of_vertices[new_connected_vertex]:
+                    # print(1)
+                new_combination_vertices.append((new_out_vertex, new_connected_vertex))
 
-        return (out_vertice, connected_vertice), new_combination_vertices
+        return (out_vertex, connected_vertex), new_combination_vertices
+
+    def get_assign_delta_inside_cluster(self, cluster, new_edge, old_edge):
+        pass
+
+    def set_value_propagate_inside_cluster(self, cluster, new_edge, old_edge):
+        pass
+
+    # def calculate_cost_inside_cluster(self, cluster, source):
+    #     # cost of source of cluster will be set to 0
+    #     # how can we find source of cluster?
+    #     # using BFS
+    #     cluster_vertices = self.R[cluster]
+    #
+    #     cost = np.zeros(self.n)
+    #     cost[source] = 0
+    #
+    #     visited = [False] * self.n
+    #
+    #     # Create a queue for BFS
+    #     queue = []
+    #
+    #     queue.append(self.source_vertex)
+    #     visited[self.source_vertex] = True
+    #
+    #     while queue:
+    #         s = queue.pop(0)
+    #
+    #         neighbors = np.where(self.x[s] == 1)[0]
+    #         neighbors = set(neighbors).intersection(set(cluster_vertices))
+    #         for i in neighbors:
+    #             if visited[i] == False:
+    #                 queue.append(i)
+    #                 visited[i] = True
+    #                 cost[i] = self.distances[s][i] + cost[s]
+    #
+    #     return cost
+
+    def find_cycle_inside_cluster(self, source):
+        # Use DFS
+        pass
 
     def get_neighbors_inside_cluster(self):
         random_cluster = np.random.randint(self.n_clusters)
@@ -241,32 +284,37 @@ class CluSTP:
         # tạo chu trình trong cluster đc chọn này, sau đó loại bỏ 1 cạnh trong chu trình để được cây khung mới
         cluster_vertices = self.R[random_cluster]
 
+        # use DFS
+
+
 
     def search(self):
         # search solution:
         # choose one leaf cluster, make change inside cluster, move out-egde of cluster to another cluster
         print("Init out vertices of cluster =", self.out_vertices_of_cluster)
+        it = 1
         while True:
             old_combination_vertices, new_combination_vertices = self.get_neighbors()
-            (out_vertice, connected_vertice) = old_combination_vertices
+            (out_vertex, connected_vertex) = old_combination_vertices
 
             deltas = []
             for combination in new_combination_vertices:
-                new_out_vertice, new_connected_vertice = combination
-                delta = self.get_assign_delta(out_vertice, connected_vertice, new_out_vertice, new_connected_vertice)
+                new_out_vertex, new_connected_vertex = combination
+                delta = self.get_assign_delta(out_vertex, connected_vertex, new_out_vertex, new_connected_vertex)
                 deltas.append(delta)
 
             min_index = np.argmin(deltas)
             # if deltas[int(min_index)] <= 0:
             chosen_combination = new_combination_vertices[int(min_index)]
-            self.set_value_propagate(out_vertice, connected_vertice, chosen_combination[0], chosen_combination[1])
-            # print("Remove", out_vertice, connected_vertice,
-            #       "from cluster", self.cluster_of_vertices[out_vertice],
-            #       "to cluster", self.cluster_of_vertices[connected_vertice],
+            self.set_value_propagate(out_vertex, connected_vertex, chosen_combination[0], chosen_combination[1])
+            # print("Remove", out_vertex, connected_vertex,
+            #       "from cluster", self.cluster_of_vertices[out_vertex],
+            #       "to cluster", self.cluster_of_vertices[connected_vertex],
             #       "\nAdd", chosen_combination[0], chosen_combination[1],
             #       "from cluster", self.cluster_of_vertices[chosen_combination[0]],
             #       "to cluster", self.cluster_of_vertices[chosen_combination[1]],)
-            print("Current Cost =", self.total_cost, "\t", "out vertices =", self.out_vertices_of_cluster)
+            print("Step", it, "\tCurrent Cost =", self.total_cost, "\t", "out vertices =", self.out_vertices_of_cluster)
+            it += 1
             # print("--------------------------------------------------------------")
 
 
